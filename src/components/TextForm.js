a120232bf8512d0a77529f100b57055ca3c4b79d
Muhammad Haseeb <muhamamdhaseeb844@gmail.com>
2024-07-29 10:37:36 -0700

    --LowerCase|no or word\character count

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,26 +1,40 @@
-import React, { useState } from 'react';
-
+import React, {useState} from 'react';
 
 
 export default function TextForm(props) {
-    const  handelUpClick = ()=> {
+    const handelUpClick = () => {
         let newText = text.toUpperCase()
         setText(newText);
     }
 
-    const  handelOnChange = (event)=> {
+    const handelLoClick = () => {
+        let newText = text.toLowerCase()
+        setText(newText);
+    }
+
+    const handelOnChange = (event) => {
         setText(event.target.value);
     }
 
     const [text, setText] = useState('Enter text here');
     return (
+        <>
+            <div className="mb-3 container">
+                <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.heading}</label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" value={text} rows="5"
+                          onChange={handelOnChange}></textarea>
+                <button className="btn btn-primary mt-3" onClick={handelUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mt-3 mx-3" onClick={handelLoClick}>Convert to lowerCase</button>
+            </div>
+            <div className="container">
+                <div className="float-end">
+                    <p>{text.split(" ").length} Words , <span className="ml-3">{text.length} Character</span></p>
+                    <p>{0.008 * text.split(" ").length} Minute Read</p>
+                </div>
 
-        <div className="mb-3">
-            <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.heading}</label>
-            <textarea className="form-control" id="exampleFormControlTextarea1" value={text} rows="5" onChange={handelOnChange}></textarea>
-            <button className="btn btn-primary mt-3" onClick={handelUpClick}>Convert to Uppercase</button>
-        </div>
-
+                <p><b>Your Content: </b>{text}</p>
+            </div>
+        </>
     )
 }
 
